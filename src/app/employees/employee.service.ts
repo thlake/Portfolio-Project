690e4b3a5e3ba86233bcf8c302a7068dbc6b2d91
Troy Lake <lakehtroy@verizon.net>
2022-01-26 20:19:01 -0500

    Adjusted environments file variables to work with digitial ocean environment variables

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Employee } from './employee';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private baseURL = "http://localhost:8080/api/v1/";
-  private secondURL = "http://localhost:8080/api/v1/employee"
+  private baseURL = environment.API_HOST + "/api/v1/";
+  private secondURL = environment.API_HOST + "/api/v1/employee"
   currentEmployee: Employee;
   
 

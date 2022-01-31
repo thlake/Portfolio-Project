543a0d92a9fa62fba700c5acbff510dd16215a2a
Troy Lake <lakehtroy@verizon.net>
2022-01-30 19:41:22 -0500

    Added port 8443 url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: "http://137.184.139.111:8080/portfolioBackend-0.0.1-SNAPSHOT"
+  API_URL: "https://137.184.139.111:8443/portfolioBackend-0.0.1-SNAPSHOT"
 };

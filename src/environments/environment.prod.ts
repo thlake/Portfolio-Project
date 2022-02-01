2b7c01d56a521eeaafed85df4f3f908fe4153907
Troy Lake <lakehtroy@verizon.net>
2022-02-01 18:23:51 -0500

    Added secure domain url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: "https://137.184.139.111:8443/portfolioBackend-0.0.1-SNAPSHOT"
+  API_URL: "https://thlportfolio.com:8443/portfolioBackend-0.0.1-SNAPSHOT"
 };
